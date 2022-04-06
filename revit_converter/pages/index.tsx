def00b6c4979850f34fc282e85e666fc732ec783
jkleinau <justus@kleinau-online.de>
2022-04-06 13:53:36 +0200

    Simple File Upload added

--- revit_converter/pages/index.tsx
+++ revit_converter/pages/index.tsx
@@ -1,10 +1,23 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import { UiFileInputButton } from '../components/UIFileInputButton';
+import axios from 'axios';
+const IndexPage = (props) => {
+	const onChange = async (formData) => {
+		const config = {
+			headers: { 'content-type': 'multipart/form-data' },
+			onUploadProgress: (event) => {
+				console.log(`Current progress:`, Math.round((event.loaded * 100) / event.total));
+			},
+		};
 
-const Home: NextPage = () => {
-	return <div className=''> Hello World Test </div>;
+		const response = await axios.post('/api/uploads', formData, config);
+
+		console.log('response', response.data);
+	};
+
+	return (
+		<div>
+			<UiFileInputButton label='Upload Single File' uploadFileName='theFiles' onChange={onChange} />
+		</div>
+	);
 };
-
-export default Home;
+export default IndexPage;
